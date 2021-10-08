744950d5e07e506ef0c24b63268a13004182f75b
LAPTOP-SBPJN31Q\01088 <01088287493@daum.net>
2021-10-08 21:12:26 +0900

    first commit

---
+++ src/main/java/edu/bit/ex/ProjectThemovieApplication.java
@@ -0,0 +1,13 @@
+package edu.bit.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectThemovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectThemovieApplication.class, args);
+	}
+
+}
